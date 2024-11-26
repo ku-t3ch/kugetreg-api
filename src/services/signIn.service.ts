295b10140cc3b94e92e1a3f78de732afe9db1b5a
Teerut26 <teerutsr@gmail.com>
2024-11-26 15:46:43 +0700

    feat: Add custom headers to sign-in service API request

--- src/services/signIn.service.ts
+++ src/services/signIn.service.ts
@@ -21,13 +21,34 @@ interface ISignInServiceInput {
 
 const SignInService = async (props: ISignInServiceInput) => {
     try {
-        const res = await axiosAPI.post<ISignInServiceResponse>("/auth/login", {
-            username: encodeString(props.username),
-            password: encodeString(props.password),
-        });
+        const res = await axiosAPI.post<ISignInServiceResponse>(
+            "/auth/login",
+            {
+                username: encodeString(props.username),
+                password: encodeString(props.password),
+            },
+            {
+                headers: {
+                    authority: "myapi.ku.th",
+                    accept: "*/*",
+                    "accept-language": "en-US,en;q=0.9,th-TH;q=0.8,th;q=0.7",
+                    "content-type": "application/json",
+                    origin: "https://my.ku.th",
+                    referer: "https://my.ku.th/",
+                    "sec-ch-ua":
+                        '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": '"macOS"',
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-site",
+                    "user-agent":
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+                },
+            }
+        );
 
         console.log(res.data);
-        
 
         return res.data;
     } catch (error) {
