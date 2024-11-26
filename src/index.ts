445d5a57f2c28441e70da5b2d340d066be9f5c19
Teerut26 <teerutsr@gmail.com>
2024-11-26 11:50:45 +0700

    Add cron job for periodic task and Docker setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { Elysia } from "elysia";
+import cron from "node-cron";
+
+// every 40 seconds
+cron.schedule("*/40 * * * * *", async () => {
+    console.log("running a task every 50 seconds for kugetreg");
+    const response = await fetch("https://kugetreg.teerut.com");
+    if (response.ok) {
+        console.log("kugetreg is running");
+    } else {
+        console.log("kugetreg is not running");
+    }
+});
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
