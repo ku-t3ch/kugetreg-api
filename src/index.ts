c7e010ddd5b7dad97c40ee01342b545a4ad877b3
Teerut26 <teerutsr@gmail.com>
2024-11-26 12:09:17 +0700

    feat: Add kugetreg API health check and refactor cron job

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import { Elysia } from "elysia";
 import cron from "node-cron";
 
-// every 40 seconds
-cron.schedule("*/40 * * * * *", async () => {
-    console.log("running a task every 50 seconds for kugetreg");
+const kugetregFetch = async () => {
     const response = await fetch("https://kugetreg.teerut.com");
     if (response.ok) {
         console.log("kugetreg is running");
     } else {
         console.log("kugetreg is not running");
     }
+};
+
+const kugetregApiFetch = async () => {
+    const response = await fetch("https://kugetreg-api-latest.onrender.com");
+    if (response.ok) {
+        console.log("kugetreg api is running");
+    } else {
+        console.log("kugetreg api is not running");
+    }
+};
+
+// every 40 seconds
+cron.schedule("*/40 * * * * *", () => {
+    console.log("running a task every 50 seconds for kugetreg");
+    kugetregFetch();
+    kugetregApiFetch();
 });
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
